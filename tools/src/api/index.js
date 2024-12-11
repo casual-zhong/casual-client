e4f77282c0944f2c5f02d4476396f6a9488f8198
钟根凤 <1781507490@qq.com>
2024-12-11 16:33:07 +0800

    1

--- tools/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import { httpGet, httpPost } from '@/utils/request.js'
-
-const mail = data => httpPost('/mail', data)
-
-const test = data => httpGet('/test', data)
-
-export default {
-    mail,test
-}
